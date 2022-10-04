60ce6e0676c0a1b406f5fed64e062eb8f23c2679
Pavan Ananth Sharma <86551444+PavanAnanthSharma@users.noreply.github.com>
2022-10-05 01:56:35 +0530

    Update Options.sol

--- GoldDust-Defi©/contracts/Options.sol
+++ GoldDust-Defi©/contracts/Options.sol
@@ -8,7 +8,7 @@
 
 // @notice This contract is in progress!!
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.7;
+pragma solidity ^0.8.0;
 
 import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
